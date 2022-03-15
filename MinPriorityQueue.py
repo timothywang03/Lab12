5fc020cc0847b0dafa2912640c8ab3b1557163a5
Timothy Wang <curioustimwang@gmail.com>
2022-03-14 22:12:28 -0700

    finalized queue

--- MinPriorityQueue.py
+++ MinPriorityQueue.py
@@ -12,8 +12,10 @@ class MinPriorityQueue:
         return self.arr[0]
 
     def minHeapify(self, i):
-        left = i * 2
-        right = i * 2 + 1
+        left = i * 2 + 1
+        right = i * 2 + 2
+
+        # finds the minimum value amongst the current, left, and right
         if left < self.size and self.arr[left] < self.arr[i]:
             smallest = left
         else:
@@ -21,20 +23,27 @@ class MinPriorityQueue:
         if right < self.size and self.arr[right] < self.arr[smallest]:
             smallest = right
 
+        # replaces the minimum values
         if smallest != i:
             mid = self.arr[i]
             self.arr[i] = self.arr[smallest]
             self.arr[smallest] = mid
 
-            self.minHeapify(self, smallest)
+            self.minHeapify(smallest)
 
     def extractMin(self):
         if self.size < 1:   # makes sure that the queue is not empty
             return 'Heap Underflow'
-        smallest = self.arr[1]
-        self.arr[1] = self.arr[self.size]
-        self.arr -= 1
-        self.minHeapify(1)
+        smallest = self.arr[0]
+        self.arr[0] = self.arr[self.size - 1]
+        self.size -= 1
+
+        # replaces the root node with the last value, removes the last value
+        self.arr[self.size] = None
+
+        # calls the organizing function to reheapify
+        self.minHeapify(0)
+        return smallest
 
     def insert(self, key):
         self.size += 1  # increments queue size to accomodate new queue entry
@@ -57,4 +66,6 @@ queue.insert(2)
 queue.insert(1)
 queue.insert(5)
 queue.insert(3)
+queue.extractMin()
+queue.extractMin()
 print(queue.arr)
